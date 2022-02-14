5f72fddc7b89aaef7d027fc9c3b94b9729bd3702
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2022-02-14 09:03:22 -0500

    submit project

---
+++ README.md
@@ -0,0 +1,5 @@
+I had a lot of trouble with this assignment. Still, I think I've learned
+a good bit since Monday. I'm trying not to be hard on myself. There are a few
+things that seem to not be clicking in terms of understanding logic and syntax.
+I did my best to not look up any tic tac toe games, and I really went off my 
+intuition of what would be right to use. I did look up some stuff regarding the .onclick function, which looks different than what we've done. I'd much rather be showing you exactly where I'm at then researching how to code for this game and applying logic I didnt get to begin with. 
--- js/app.js
+++ js/app.js
@@ -1,17 +1,74 @@
+//create variables to reference 9 boxes each with specific ID
 
-const generateBox1Buttons = document.querySelector('#box1');
-const generateBox1Buttons = document.querySelector('#box2');
+const reset = document.querySelector('#reset')
+const message = document.querySelector('#message')
+const playAgainButton = document.querySelector('#play-again')
+const Box1 = document.querySelector('#box1')
+const Box2 = document.querySelector('#box2')
+const Box3 = document.querySelector('#box3')
+const Box4 = document.querySelector('#box4')
+const Box5 = document.querySelector('#box5')
+const Box6 = document.querySelector('#box6')
+const Box7 = document.querySelector('#box7')
+const Box8 = document.querySelector('#box8')
+const Box9 = document.querySelector('#box9')
+
+//Was playing around with querySelectorAll to reference
+//all of them via class
+//const Boxes = document.querySelectorAll('.childDiv')
+
+//create an array to store the querySelector for each box
+const boxes = [Box1,Box2,Box3,Box4,Box5,Box6,Box7,Box8,Box9]
+
+////const boxesId = ['#box1','#box2','#box3','#box4','#box5','#box6','#box7','#box8','#box9']
+
+//create a function for appending a child into the div
+//so it loops through the nine boxes referenced in the array
+
+const playerMove = () => { 
+    //didn't get to figure out how to apply the logic
+    //to switch values
+   let player1 = "o"
+   let player2 = "x"
+   
+   for (let i = 0; i < boxes.length; i++) {
+    //console.log(boxes[i]) 
+        boxes[i].onclick = function () {
+        addXtoBox = document.createElement("p")
+        addXtoBox.innerText = "x"
+        boxes[i].appendChild(addXtoBox)
+        //create a condition so the child that's appended
+        //doesn't repeat whenever I click
+            if(boxes[i].firstChild){
+            boxes[i].removeChild(boxes[i].firstChild)
+            }
+        }
+    }
+}  
+
+//create a variable to generate game once the game ends, include
+//an innerText for the <p> element in html that has a specific ID
+//so the user is instructed to play again
+
+const generateGame = () => {
+message.innerText = "Make your move!"
+// I tried playing around with the playerMove() function and placing it 
+//because I was having issues being able to append children to the divs
+//once the game is cleared.
+playerMove()
+}
+
+const clearGame = () => {
+    for (let i = 0;i < boxes.length;i++){
+       boxes[i].removeChild(boxes[i].firstChild)
+    }
+message.innerText = 'game successfully cleared'
+}
 
 
+document.addEventListener('DOMContentLoaded', () => {
+playerMove()
+reset.addEventListener('click', clearGame)
+playAgainButton.addEventListener('click',generateGame)
 
-const buttonX = document.createElement("button")
-buttonX.classList.add("butt1")
-//button.type = "submit"
-buttonX.innerHTML = "X"
-generateBox1Buttons.appendChild(buttonX)
-
-const buttonO = document.createElement("button")
-buttonO.classList.add("butt1")
-//button.type = "submit"
-buttonO.innerHTML = "O"
-generateBox1Buttons.appendChild(buttonO)
+})
